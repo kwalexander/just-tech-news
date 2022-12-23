d46e5987f9ea8947e0066dacd5fad14741c64b17
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-12-23 12:52:46 -0800

    login and logout

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,6 @@
-// const router = require('express').Router();
-// const homeRoutes = require('./home-routes.js');
-
-// const apiRoutes = require('./api');
-// const { route } = require('./api');
-
-// router.use('/api', apiRoutes);
-
-// router.use((req, res) => {
-//   res.status(404).end();
-// });
-
-// router.use('/', homeRoutes);
-
-// module.exports = router;
-
-
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
